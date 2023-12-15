144690b42f22be6d579bedebf526c8f3ae50b647
sukhbir.singh <sukhbir.singh@netsolutions.com>
2023-12-15 11:42:41 +0530

    added readme.md file and updated register fn code

--- src/firebaseAuth/FirebaseAuth.php
+++ src/firebaseAuth/FirebaseAuth.php
@@ -2,16 +2,22 @@
 namespace FirebaseAuth;
 
 use GuzzleHttp\Client;
-//update class
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ConnectException;
+
+
+
 class FirebaseAuth
 {
-    private static $baseUrl = 'https://identitytoolkit.googleapis.com/v1/';
-    private static $apiKey;
-
-
-    public static function initialize($apiKey)
+    
+    private static $baseUrl;
+    private static $authToken;
+   
+    
+    public static function initialize($authToken)
     {
-        self::$apiKey = $apiKey;
+        self::$authToken = $authToken;
+        self::$baseUrl = env('NODE_DOMAIN','http://localhost');
     }
 
     private static function getClient()
@@ -19,18 +25,36 @@ class FirebaseAuth
         return new Client();
     }
 
-    public static function register($email, $password)
+    public static function register($display_name, $email)
     {
         $client = self::getClient();
-        $response = $client->post(self::$baseUrl . 'accounts:signUp?key=' . self::$apiKey, [
-            'json' => [
-                'email' => $email,
-                'password' => $password,
-                // Add other parameters as needed
-            ],
-        ]);
+        $signUpApiUrl = self::$baseUrl .'registration';
+        try{
+            $response = $client->post($signUpApiUrl, [
+                'headers' => [
+                    'authorization' => self::$authToken,
+                ],
+                'json' => [
+                    'email' => $email,
+                    'displayName' => $display_name,
+                ]
+            ]);
+            return json_decode($response->getBody(), true);
 
-        return json_decode($response->getBody(), true);
+        }catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                $response = $e->getResponse();
+                $statusCode = $response->getStatusCode();
+                return json_decode($response->getBody(), true);
+            }else{
+                return json_decode(json_encode(["message"=> $e->getMessage(),"status_code"=> $e->getCode()]), true);
+            }
+        }catch (ConnectException $e) {
+           return json_decode(json_encode(["message"=> "Service not available","status_code"=> 503]), true);
+        }catch(\Exception $e){
+            return json_decode(json_encode(["message"=> $e->getMessage(),"status_code"=> $e->getCode()]), true);
+        }
+        
     }
 
     
