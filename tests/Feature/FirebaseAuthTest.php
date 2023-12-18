a7e641255103ef63b0b3e9a042ae30dbfcfd5c78
sukhbir.singh <sukhbir.singh@netsolutions.com>
2023-12-18 17:33:33 +0530

    Updates in SDK files

---
+++ tests/Feature/FirebaseAuthTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+use PHPUnit\Framework\TestCase;
+
+class FirebaseAuthTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+    public function test_the_application_returns_a_successful_response(): void
+    {
+       $this->assertTrue(true);
+    }
+}
