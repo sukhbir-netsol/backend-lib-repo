4b53d818aa5d4b4c3cf36d604d9e45708199efb2
sukhbir.singh <sukhbir.singh@netsolutions.com>
2024-01-02 15:30:51 +0530

    fix: added return type of customvalidator class functions

--- src/FirebaseAuth/CustomValidator.php
+++ src/FirebaseAuth/CustomValidator.php
@@ -2,7 +2,8 @@
 namespace FirebaseAuth;
 
 class CustomValidator {
-    public static function validateEmail($email) {
+    public static function validateEmail($email) :bool
+    {
         // Use PHP filter_var function to validate email
         $emailIsValid = false;
         if (!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -10,7 +11,8 @@ class CustomValidator {
         } 
         return $emailIsValid;
     }
-    public static function validateName($name) {
+    public static function validateName($name) : bool
+    {
         // Use a regular expression to validate name (allowing only letters and spaces)
         $nameIsValid = false;
         if (!empty($name) && preg_match('/^[a-zA-Z\s]+$/', $name)) {
